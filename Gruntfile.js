a77ddd4efd512592980901f6e4cb7866c8be8b7a
Simon Jesenko <simon.jesenko@gmail.com>
2014-12-31 12:52:24 +0100

    fix to work with latest bower version

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,23 +10,31 @@ module.exports = function(grunt) {
         copy: {
             'default': {
                 files: {
-                    'examples/dist/':
-                        [].concat(
-                            collect.select('example-lib.css'),
-                            collect.select('example-lib.js')
-                        )
+                    'examples/dist/jquery.js': 'bower_components/jquery/dist/jquery.js',
+                    'examples/dist/bootstrap.css': 'bower_components/bootstrap/dist/css/bootstrap.css',
+                    'examples/dist/jquery.ui.core.js': 'bower_components/jquery-ui/ui/core.js',
+                    'examples/dist/jquery.ui.widget.js': 'bower_components/jquery-ui/ui/widget.js',
+                    'examples/dist/jquery.ui.mouse.js': 'bower_components/jquery-ui/ui/mouse.js',
+                    'examples/dist/jquery.ui.resizable.js': 'bower_components/jquery-ui/ui/resizable.js',
+                    'examples/dist/jquery.ui.sortable.js': 'bower_components/jquery-ui/ui/sortable.js',
+                    'examples/dist/slick.core.js': 'bower_components/slickgrid/slick.core.js',
+                    'examples/dist/slick.grid.js': 'bower_components/slickgrid/slick.grid.js',
+                    'examples/dist/slick.dataview.js': 'bower_components/slickgrid/slick.dataview.js',
+                    'examples/dist/slick.rowselectionmodel.js': 'bower_components/slickgrid/plugins/slick.rowselectionmodel.js',
+                    'examples/dist/slick.checkboxselectcolumn.js': 'bower_components/slickgrid/plugins/slick.checkboxselectcolumn.js',
+                    'examples/dist/jquery.event.drag-2.2.js': 'bower_components/slickgrid/lib/jquery.event.drag-2.2.js',
+                    'examples/dist/slick.grid.css': 'bower_components/slickgrid/slick.grid.css'
                 }
             }
         },
         less: {
             'default': {
                 options: {
-                    paths: ['examples', 'bootstrap', 'components/bootstrap/less'],
+                    paths: ['examples', 'bootstrap', 'bower_components/bootstrap/less'],
                     yuicompress: false
                 },
                 files: {
-                    'examples/dist/example-bootstrap.css':
-                        collect.select('example-bootstrap.css')
+                    'examples/dist/example-bootstrap.css': 'examples/example-bootstrap.less'
                 }
             }
         },
@@ -35,15 +43,10 @@ module.exports = function(grunt) {
                 options: {
                     debounceDelay: 250
                 },
-                files: [].concat(
-                    collect.select('example-bootstrap.css'),
-                    collect.select('example-lib.css'),
-                    collect.select('example-lib.js'),
-                    [
+                files: [
                         'bootstrap/slickgrid.less',
-                        'components/bootstrap/less/variables.less'
-                    ]
-                ),
+                        'bower_components/bootstrap/less/variables.less'
+                        ],
                 tasks: ['copy:default', 'less:default']
             }
         }
